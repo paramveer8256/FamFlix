b25eb543ff4344c28eb4cb4d89be2cf791500194
Paramveer Singh <paramveer7267@gmail.com>
2025-04-05 16:39:17 +0530

    ndw

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://famflix.onrender.com",
+        target: "http://localhost:5000",
       },
     },
   },
